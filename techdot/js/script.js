bf8e7ed1f414839cce3bc034d2f6671ba2600f67
Lakshmanan1996 <117020820+Lakshmanan1996@users.noreply.github.com>
2025-09-27 12:04:03 +0530

    Update and rename techdot/script.js to techdot/js/script.js

---
+++ techdot/js/script.js
@@ -0,0 +1,25 @@
+// Typing effect
+const text = ["Front-End Developer", "React & JS Enthusiast", "Tech Lover"];
+let i = 0, j = 0, currentText = "", isDeleting = false;
+function type() {
+  if(i < text.length){
+    if(!isDeleting && j <= text[i].length){
+      currentText = text[i].substring(0,j++);
+      document.querySelector(".typing-text").textContent=currentText;
+      setTimeout(type,150);
+    } else if(isDeleting && j >=0){
+      currentText = text[i].substring(0,j--);
+      document.querySelector(".typing-text").textContent=currentText;
+      setTimeout(type,100);
+    } else {
+      isDeleting=!isDeleting;
+      if(!isDeleting)i++;
+      if(i>=text.length)i=0;
+      setTimeout(type,500);
+    }
+  }
+}
+type();
+
+// Particles.js
+particlesJS.load('particles-js','particles.json',function(){console.log('particles loaded');});
