289112a6126c844fa78f74a4a83ec9f50a3665c5
Lakshmanan1996 <117020820+Lakshmanan1996@users.noreply.github.com>
2025-09-28 16:41:54 +0530

    Update script.js

--- techdata/script.js
+++ techdata/script.js
@@ -3,8 +3,8 @@ const typingTextElement = document.querySelector('.typing-text');
 const textToType = "Cloud Engineer";
 
 // Define Speeds (in milliseconds)
-const TYPING_SPEED = 15;  // Your requested typing speed
-const BACK_SPEED = 05;    // Your requested backspacing speed
+const TYPING_SPEED = 5;  // Your requested typing speed
+const BACK_SPEED = 50;    // Your requested backspacing speed
 const PAUSE_DELAY = 500;  // Pause before backspacing
 
 let charIndex = 0;
